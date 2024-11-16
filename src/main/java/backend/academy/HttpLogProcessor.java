fc5d4c464e77dddedc3a2c38f6626ac3606b57c4
Alexander <avsolovkin@edu.hse.ru>
2024-11-16 20:14:15 +0300

    feat(code): Add LogProcessors http and local implementations

---
+++ src/main/java/backend/academy/HttpLogProcessor.java
@@ -0,0 +1,44 @@
+package backend.academy;
+
+import lombok.extern.log4j.Log4j2;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.LocalDate;
+import java.util.stream.Stream;
+
+/// Класс для чтения веб-файла и получения из него потока с логами.
+@Log4j2
+public class HttpLogProcessor implements LogProcessor {
+
+    @Override
+    public void getLogStream(String path, LocalDate fromDate, LocalDate toDate) {
+
+        try (HttpClient client = HttpClient.newHttpClient()) {
+
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(path))
+                .GET()
+                .build();
+
+            try {
+                try (BufferedReader reader = new BufferedReader(
+                    new InputStreamReader(client.send(request, HttpResponse.BodyHandlers.ofInputStream()).body()))) {
+                    try (Stream<String> lines = reader.lines()) {
+                        Stream<LogRecord> records = lines.map(LogProcessor::parseLogLine);
+                        logAnalyzer.updateLogReport(path, fromDate, toDate, records);
+                    }
+                }
+
+            } catch (IOException | InterruptedException ex) {
+                log.error(ex.getMessage());
+            }
+        } catch (Exception ex) {
+            log.error(ex.getMessage());
+        }
+    }
+}
---
+++ src/main/java/backend/academy/LocalLogProcessor.java
@@ -0,0 +1,41 @@
+package backend.academy;
+
+import lombok.extern.log4j.Log4j2;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.PathMatcher;
+import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.util.stream.Stream;
+
+/// Класс для чтения локальных файлов по glob-паттерну и получения из них потока с логами.
+@Log4j2
+public class LocalLogProcessor implements LogProcessor {
+
+    @Override
+    public void getLogStream(String path, LocalDate fromDate, LocalDate toDate) {
+        String baseDir = "";
+
+        try {
+            Path baseDirPath = Paths.get(baseDir).normalize();
+            PathMatcher matcher = baseDirPath.getFileSystem().getPathMatcher("glob:" + path);
+
+            try (Stream<Path> paths = Files.walk(baseDirPath)) {
+                paths.filter(matcher::matches).forEach(p -> {
+                    try (Stream<String> lines = Files.lines(p, StandardCharsets.UTF_8)) {
+                        Stream<LogRecord> records = lines.map(LogProcessor::parseLogLine);
+                        logAnalyzer.updateLogReport(p.toString(), fromDate, toDate, records);
+                    } catch (IOException ex) {
+                        log.error("0");
+                    }
+                });
+            } catch (IOException ex) {
+                log.error(ex.getMessage());
+            }
+        } catch (Exception ex) {
+            log.error(ex.getMessage());
+        }
+    }
+}
