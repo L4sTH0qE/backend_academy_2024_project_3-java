7205d0c91d15298e2101e97589f462e544e54281
Alexander <avsolovkin@edu.hse.ru>
2024-11-17 13:41:57 +0300

    feat(code): Add file writer interface

---
+++ src/main/java/backend/academy/writer/LogWriter.java
@@ -0,0 +1,13 @@
+package backend.academy.writer;
+
+import backend.academy.model.LogReport;
+
+/// Интерфейс для классов, реализующих способ генерации файла заданного формата с собранной по логам статистикой.
+public interface LogWriter {
+
+    // Используемый для отчета перцентиль - 95-ый.
+    int percentile = 95;
+
+    /// Метод для создания файла с собранной по логам статистикой.
+    void generateFile(LogReport logReport);
+}
